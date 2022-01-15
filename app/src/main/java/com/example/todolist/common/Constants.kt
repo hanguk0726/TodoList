226297e1c734743a1ae4b316ccf93c12356fc207
hanguk <hanguk0726@gmail.com>
2022-01-15 19:40:09 +0900

    _

--- app/src/main/java/com/example/todolist/common/Constants.kt
+++ app/src/main/java/com/example/todolist/common/Constants.kt
@@ -4,6 +4,7 @@ import android.provider.Settings
 
 object Constants {
 
-    const val BASE_URL = "http://192.168.123.102:8080"
+//    const val BASE_URL = "http://192.168.123.102:8080"
+    const val BASE_URL = "https://todolist-server-nemo.herokuapp.com"
 
 }
